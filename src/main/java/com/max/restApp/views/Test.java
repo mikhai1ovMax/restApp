f18451dc7de224698c9b370ca6392fe45c270ba2
maxim-creator <max47166978@gmail.com>
2021-05-24 10:01:16 +0300

    add Procfile

--- src/main/java/com/max/restApp/views/Test.java
+++ src/main/java/com/max/restApp/views/Test.java
@@ -8,16 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(value = "/main")
-public class MainPage extends HttpServlet {
+@WebServlet("/test")
+public class Test extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html");
         PrintWriter messageWriter = response.getWriter();
-        messageWriter.print("<html><body>");
-        messageWriter.println("<h1>" + "mainPage" + "<h1>");
-        messageWriter.print("</body></html>");
+        messageWriter.write("hi");
         messageWriter.close();
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
 }
